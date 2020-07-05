62feed4f62133fee6c0bdada6b33a18909944219
Ruchi <ruchiarora2348@gmail.com>
2020-07-05 16:40:15 +0530

    inital commit

---
+++ src/test/java/com/automation/webdriverMaven/LoginTest.java
@@ -0,0 +1,37 @@
+package com.automation.webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	
+	public void setUp()
+	{
+		if(driver==null)
+		{
+			System.setProperty("webdriver.chrome.driver","E:\\work\\chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver","E:\\work\\chrome\\chromedriver.exe");
+			System.out.println("test");
+			driver=new ChromeDriver();
+			driver.get("https://www.facebook.com/");
+			driver.manage().window().maximize();
+		}
+	}
+  @Test	
+public void login()
+	
+	{
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys("test@test.com");
+		driver.findElement(By.id("pass")).clear();
+		driver.findElement(By.id("pass")).sendKeys("test");
+		driver.findElement(By.id("loginbutton")).click();
+	}
+}
